01f81b658dfdbad447a809868acf6da79b25f90c
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-12-08 18:34:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let pannel=document.querySelectorAll(".panel");
+pannel.forEach((e)=>{
+  e.addEventListener("click",func)
+})
+function func(e)
+{
+  document.querySelectorAll(".panel").forEach((item)=>{
+    if(item.classList.contains("active"))
+    {
+      item.classList.remove("active")
+    }
+  })
+  // e.classList().toggle("active");
+  e.target.classList.add("active");
+  // e.target.children[0].style.display="block";
+}
